966835292ebc306c6e10db349569e7e1055120d3
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2024-06-08 21:24:16 +0300

    final

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+const button = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+const body = document.querySelector("body");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+button.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+  color.innerText = `-${newColor}`;
+});
